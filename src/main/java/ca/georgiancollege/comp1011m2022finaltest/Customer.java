250badad1bcdbb6fdec4e6e57b6fd68eebbfc004
SFA01 <sarafranzinoandrade@hotmail.com>
2022-08-21 18:59:15 -0400

    Customer + Product added.

--- src/main/java/ca/georgiancollege/comp1011m2022finaltest/Customer.java
+++ src/main/java/ca/georgiancollege/comp1011m2022finaltest/Customer.java
@@ -1,5 +1,61 @@
-package com.example.w22comp1011gctest2student;
+package ca.georgiancollege.comp1011m2022finaltest;
 
 public class Customer {
 
+    //Here I am doing the private instances members
+    private int id;
+    private String name;
+    private String lastname;
+    private String PhoneNumber;
+    private String Products;
+
+    //Here I am doing the getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getProducts() {
+        return Products;
+    }
+
+    public void setProducts(String products) {
+        Products = products;
+    }
+
+    //Here I am doing the constructors
+    public Customer(int id, String name, String lastname, String phoneNumber, String products) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        PhoneNumber = phoneNumber;
+        Products = products;
+    }
 }
--- src/main/java/ca/georgiancollege/comp1011m2022finaltest/Product.java
+++ src/main/java/ca/georgiancollege/comp1011m2022finaltest/Product.java
@@ -1,4 +1,59 @@
-package com.example.w22comp1011gctest2student;
+package ca.georgiancollege.comp1011m2022finaltest;
 
 public class Product {
+
+  //Here I am doing the private instances members
+  private int Sku;
+  private String Name;
+  private double SalePrice;
+  private double RegularPrice;
+  private String UrlInformation;
+
+  //Here I am doing the getters and setters
+
+  public int getSku() {
+    return Sku;
   }
+
+  public void setSku(int sku) {
+    Sku = sku;
+  }
+
+  public String getName() {
+    return Name;
+  }
+
+  public void setName(String name) {
+    Name = name;
+  }
+
+  public double getSalePrice() {
+    return SalePrice;
+  }
+
+  public void setSalePrice(double salePrice) {
+    SalePrice = salePrice;
+  }
+
+  public double getRegularPrice() {
+    return RegularPrice;
+  }
+
+  public void setRegularPrice(double regularPrice) {
+    RegularPrice = regularPrice;
+  }
+
+  public String getUrlInformation() {
+    return UrlInformation;
+  }
+
+  public void setUrlInformation(String urlInformation) {
+    UrlInformation = urlInformation;
+  }
+
+  //Here I am doing an override method
+  @Override
+  public String toString() {
+    return Name + SalePrice;
+  }
+}
